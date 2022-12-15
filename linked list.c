b1be2dcdca3259df307f7e34e107f6451e8864f1
Nikhil Prakash <115891140+nikhilljpskj@users.noreply.github.com>
2022-12-15 10:37:04 +0530

    Update linked list.c

--- linked list.c
+++ linked list.c
@@ -1,181 +1,218 @@
 #include<stdio.h>
 #include<stdlib.h>
-#include<malloc.h>
 struct node
 {
 int data;
-struct node *LINK;
+struct node*next;
 };
-struct node *header = NULL;
-void InsertFirst()
-{
-struct node *ptr;
-ptr=(struct node *)malloc(sizeof(struct node *));
-if(ptr==NULL)
-printf("\nNo space ");
-else{
-printf("\nEnter the element to be inserted : ");
-scanf("%d",&ptr->data);
-ptr->LINK=NULL;
-if(header==NULL)
-header=ptr;
-else{
-ptr->LINK=header;
-header=ptr;
-}
-printf("\n%d inserted into the list",ptr->data);
-}
-}
-void InsertLast()
-{
-struct node *temp,*ptr;
-ptr=(struct node *)malloc(sizeof(struct node *));
-ptr->LINK=NULL;
-temp=header;
-if(ptr==NULL)
-printf("\nNo space available ");
-else
-{
-while(temp->LINK!=NULL)
-temp=temp->LINK;
-printf("\nEnter the element to be inserted : ");
-scanf("%d",&ptr->data);
-temp->LINK=ptr;
-printf("\n%d inserted ",ptr->data);
-}
-}
-void InsertLocation()
-{
-int key;
-struct node *temp,*ptr;
-ptr=(struct node *)malloc(sizeof(struct node *));
-ptr->LINK=NULL;
-printf("\nEnter the value of the node after which the new node to be inserted : ");
-scanf("%d",&key);
-temp=header;
-while(temp->data!=key){
-temp=temp->LINK;
-if(temp==NULL)
-break;
-}
-if(temp==NULL){
-printf("The %d value not exist ",key);
-return;
-}
-if(temp->data==key){
-if(ptr==NULL)
-printf("\nNo space ");
-else{
-printf("\nEnter the element to be inserted : ");
-scanf("%d",&ptr->data);
-ptr->LINK=temp->LINK;
-temp->LINK=ptr;
-printf("\n%d inserted after %d ",ptr->data,key);
-}
-}
-}
-void DeleteFirst()
-{
-struct node *temp;
-temp=header;
-header=temp->LINK;
-printf("%d deleted ",temp->data);
-free(temp);
-}
-void DeleteLast()
-{
-struct node *temp,*p;
-temp=header;
-while(temp->LINK!=NULL)
-{
-p=temp;
-temp=temp->LINK;
-}
-printf("%d deleted ",temp->data);
-free(temp);
-p->LINK=NULL;
-}
-void DeleteLocation()
-{
-struct node *temp,*p;
-int key;
-printf("\nEnter the value of the node to be deleted : ");
-scanf("%d",&key);
-temp=header;
-while(temp->data!=key)
-{
-p=temp;
-temp=temp->LINK;
-if(temp==NULL)
-break;
-}
-if(temp==NULL){
-printf("The %d value not exist ",key);
-return;
-}
-if(temp->data==key){
-printf("%d deleted ",temp->data);
-p->LINK=temp->LINK;
-free(temp);
-}
-}
-void Search()
-{
-struct node *temp;
-int key,pos=0;
-temp=header;
-printf("\nEnter the element to be searched");
-scanf("%d",&key);
-while(temp->data!=key)
-{
-temp=temp->LINK;
-pos++;
-if(temp==NULL)
-break;
-}
-if(temp==NULL){
-printf("\nThe %d value not exist ",key);
-return;
-}
-if(temp->data==key){
-printf("\n%d is available in location '%d'",key,pos+1);
-}
-}
-void Display()
-{
-struct node *p;
-if(header==NULL)
-printf("List is empty");
-else{
-printf("\nElements in link list are : ");
-p=header;
-while(p!=NULL){
-printf("\t%d",p->data);
-p=p->LINK;
-}
-}
-}
+struct node*head;
+void beginsert();
+void lastinsert();
+void randominsert();
+void begin_delete();
+void last_delete();
+void random_delete();
+void display();
+void search();
 void main()
 {
-int choice;
-printf("\nSINGLY LINKED LIST \n");
-do
+int choice=0;
+while(choice!=8)
 {
-printf("\n\n1.Insert in begining\n2.Insert at last\n3.Insert at any random location\n4.Delete from Beginning\n5.Delete from last\n6.Delete node after specified location\n7.Search for an element\n8.Display\n9.Exit");
-printf("\nEnter a choice ");
-scanf("%d",&choice);
+printf("\n1.Insert in begining\n2.Insert at last\n3.Insert at any random location\n4.Delete from Beginning\n  5.Delete from last\n6.Delete node after specified location\n7.Show\n8.Exit\n");
+printf("\nEnter your choice?\n");
+scanf("\n%d",&choice);
 switch(choice)
 {
-case 1:InsertFirst();break;
-case 2:InsertLast();break;
-case 3:InsertLocation();break;
-case 4:DeleteFirst();break;
-case 5:DeleteLast();break;
-case 6:DeleteLocation();break;
-case 7:Search();break;
-case 8:Display();break;
-case 9:exit(0);
-default: printf("\n invalid option");
+case 1:
+beginsert();
+break;
+case 2:
+lastinsert();
+break;
+case 3:
+randominsert();
+break;
+case 4:
+begin_delete();
+break;
+case 5:
+last_delete();
+break;
+case 6:
+random_delete();
+break;
+case 7:
+display();
+break;
+case 8:
+exit(0);
+break;
+default:
+printf("Please enter valid choice..");
 }
-}while(choice!=9);
+}
+}
+void beginsert()
+{
+struct node*ptr;
+int item;
+ptr=(struct node*)malloc(sizeof(struct node*));
+if(ptr==NULL)
+{
+printf("\nOVERFLOW");
+}
+else
+{
+printf("\nEnter value\n");
+scanf("%d",&item);
+ptr->data=item;
+ptr->next=head;
+head=ptr;
+printf("\nNode inserted");
+}
+}
+void lastinsert()
+{
+struct node*ptr,*temp;
+int item;
+ptr=(struct node*)malloc(sizeof(struct node));
+if(ptr==NULL)
+{
+printf("\nOVERFLOW");
+}
+else
+{
+printf("\nEnter value?\n");
+scanf("%d",&item);
+ptr->data=item;
+if(head==NULL)
+{
+ptr->next=NULL;
+head=ptr;
+printf("\nNode inserted");
+}
+else
+{
+temp=head;
+while(temp->next!=NULL)
+{
+temp=temp->next;
+}
+temp->next=ptr;
+ptr->next=NULL;
+printf("\nNode inserted");
+}
+}
+}
+void randominsert()
+{
+int i,loc,item;
+struct node*ptr,*temp;
+ptr=(struct node*)malloc(sizeof(struct node));
+if(ptr==NULL)
+{
+printf("\nOVERFLOW");
+}
+else
+{
+printf("\nEnter element value");
+scanf("%d",&item);
+ptr->data=item;
+printf("\nEnter the location after which you want to insert ");
+scanf("\n%d",&loc);
+temp=head;
+for(i=0;i<loc;i++)
+{
+temp=temp->next;
+if(temp==NULL)
+{
+printf("\ncan't insert\n");
+return;
+}
+}
+ptr->next=temp->next;
+temp->next=ptr;
+printf("\nNode inserted");
+}
+}
+void begin_delete()
+{
+struct node*ptr;
+if(head==NULL)
+{
+printf("\nList is empty\n");
+}
+else
+{
+ptr=head;
+head=ptr->next;
+free(ptr);
+printf("\nNode deleted from the begining \n");
+}
+}
+void last_delete()
+{
+struct node*ptr,*ptr1;
+if(head==NULL)
+{
+printf("\nlist is empty");
+}
+else if(head->next==NULL)
+{
+head=NULL;
+free(head);
+printf("\nOnly node of the list deleted\n");
+}
+else
+{
+ptr=head;
+while(ptr->next!=NULL)
+{
+ptr1=ptr;
+ptr=ptr->next;
+}
+ptr1->next=NULL;
+free(ptr);
+printf("\nDeleted Node from the last ...\n");
+}
+}
+void random_delete()
+{
+struct node*ptr,*ptr1;
+int loc,i;
+printf("\n Enter the location of the node after which you want to perform deletion \n");
+scanf("%d",&loc);
+ptr=head;
+for(i=0;i<loc;i++)
+{
+ptr1=ptr;
+ptr=ptr->next;
+if(ptr==NULL)
+{
+printf("\nCan't delete");
+return;
+}
+}
+ptr1->next=ptr->next;
+free(ptr);
+printf("\nDeleted node %d ",loc+1);
+}
+void display()
+{
+struct node*ptr;
+ptr=head;
+if(ptr==NULL)
+{
+printf("Nothing to print");
+}
+else
+{
+printf("\nprinting values . . . . .\n");
+while(ptr!=NULL)
+{
+printf("\n%d",ptr->data);
+ptr=ptr->next;
+}
+}
 }
